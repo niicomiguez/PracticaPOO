9f8585a4c03aeabdf8c2d992d27ec86699784cd5
niicomiguez <niicomiguez10@gmail.com>
2026-03-12 10:04:25 +0100

    Primer commit proyecto POO Bluej

---
+++ TipoMotor.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class TipoMotor - write a description of the enum class here
+ * 
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum TipoMotor
+{
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
---
+++ TipoTapiceria.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class TipoTapiceria here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TipoTapiceria
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class TipoTapiceria
+     */
+    public TipoTapiceria()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
